68bef1b181454000df35e2f2dc6ea24ed3fc74a8
Mark Alexieff <mark.alexieff@outlook.com>
2020-03-08 21:44:11 -0700

    Works - no error checking.

--- Ch 8 ex 10/Source.cpp
+++ Ch 8 ex 10/Source.cpp
@@ -1,4 +1,5 @@
 /*  Program Description
+10. Write a function maxv() that returns the largest element of a vector argument.
 */
 
 /*
@@ -17,6 +18,9 @@ global variables
 functions declarations
 */
 
+double maxv(vector<double>& vd);
+
+
 
 
 /*
@@ -24,6 +28,16 @@ main
 */
 
 int main() {
+	vector<double> vdouble (0);
+	double temp_value{ 0 };
+	cout << "Enter a list of doubles (999 to exit): ";
+	cin >> temp_value;
+	while (temp_value != 999) {
+		vdouble.push_back(temp_value);
+		cin >> temp_value;
+	}
+	cout << "Max value = " << maxv(vdouble);
+	keep_window_open();
 
 }
 
@@ -32,4 +46,12 @@ int main() {
 funcion definitions
 */
 
-
+double maxv(vector<double>& vd) {
+	double temp_max{ 0 };
+	for (double v : vd) {
+		if (v >= temp_max) {
+			temp_max = v;
+		}
+	}
+	return temp_max;
+}
